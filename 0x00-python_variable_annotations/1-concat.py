f45a195f70b46299e631154d717c25328e34293a
bereket-kume <bereketkume@gmail.com>
2024-08-01 14:22:29 +0300

    done

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -8,5 +8,6 @@
 def concat(str1: str, str2: str) -> str:
     """
         function concat that takes a string str1 
+        a string str2 as arguments and returns a concatenated string
     """
     return str1 + str2
