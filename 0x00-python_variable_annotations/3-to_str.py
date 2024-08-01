4cbcc42badeaa2b6ffb1e0cfc59797de30ca1bde
bereket-kume <bereketkume@gmail.com>
2024-08-01 14:39:38 +0300

    done

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
+"""
+    unction to_str that takes a float n as argument 
+    and returns the string representation of the float
+"""
+
 
 def to_str(n: float) -> str:
+    """
+    Args:
+        n: is the first paramater in our function
+    return:
+        the string representation of the float.
+    """
+
     return str(n)
