4ed31ac2893499058cc2e759d55594da9c225dfe
bereket-kume <bereketkume@gmail.com>
2024-08-01 14:27:58 +0300

    done

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """
-    function concat that takes a string str1 and 
-    a string str2 as arguments and returns a concatenated string
+    function concat that takes a string str1
 """
 
 
@@ -14,4 +13,5 @@ def concat(str1: str, str2: str) -> str:
         Return:
             Concatenated string
     """
+
     return str1 + str2
