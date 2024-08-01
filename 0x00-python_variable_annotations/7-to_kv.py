ef8bf1260e4df699bbbb48bc276819bdb01f9b17
bereket-kume <bereketkume@gmail.com>
2024-08-01 14:59:46 +0300

    done

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python3
+"""
+    Mixed Tuple
+"""
 from typing import Union
 
 
 def to_kv(k: str, v: Union[int, float]) -> tuple:
+    """
+        Args:
+            k: String
+            v: Union: Can be int or float
+
+        Return:
+            Tuple with string and int or float
+    """
+
     return (k, v**2)
--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
+"""
+    Callable Function
+"""
 from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+        Args:
+            multiplier: factor
+
+        Return:
+            multiplication in float
+    """
 
     def multiplier_function(value: float) -> float:
+        """Get the second argument somthing like JS """
         return multiplier * value
 
     return multiplier_function
