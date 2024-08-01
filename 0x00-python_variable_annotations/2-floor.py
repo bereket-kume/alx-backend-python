9b01604d081b36c6ea99c07fd131608157bdb3b2
bereket-kume <bereketkume@gmail.com>
2024-08-01 14:31:45 +0300

    done

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
+"""
+    function floor which takes a float n as argument and returns the floor of the float
+"""
+
 
 def floor(n: float) -> int:
+    """
+        Args:
+            n: is the only paramter
+
+        return:
+            the floor of the float
+    """
+
     return int(n // 1)
