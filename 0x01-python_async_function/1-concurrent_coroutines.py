1137108cf8526e5a1e517c2e51deee3b3e281848
bereket-kume <bereketkume@gmail.com>
2024-08-05 14:53:34 +0300

    done

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,10 +2,11 @@
 """
     module that work with wait_random
 """
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
         function that takes two argument
         n and max+delay and create list
