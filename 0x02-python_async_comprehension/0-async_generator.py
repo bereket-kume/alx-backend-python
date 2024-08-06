7fe48155be1d3216f3a03aac50a48b27da4369ac
bereket-kume <bereketkume@gmail.com>
2024-08-06 16:43:56 +0300

    type annotation

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,9 +9,9 @@ from typing import AsyncGenerator
 
 async def async_generator() -> AsyncGenerator[float, None]:
     """
-        function that generate random number using
+        Coroutine that generate random number using
         async generator
     """
-    for _ in range(0, 10):
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,10 +2,11 @@
 """
     module for async_comprehension
 """
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """
         async_comprehension is function that create
         list of random number form async_genertor module
