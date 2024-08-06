3e3abe53e41b6c5655ec8c55788c8ccac793cbea
bereket-kume <bereketkume@gmail.com>
2024-08-06 16:14:34 +0300

    error fixed

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,14 +4,15 @@
 """
 import random
 import asyncio
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """
         function that generate random number using
         async generator
     """
-    for i in range(1, 10):
+    for _ in range(10):
         data = random.uniform(1, 10)
         yield data
         await asyncio.sleep(1)
