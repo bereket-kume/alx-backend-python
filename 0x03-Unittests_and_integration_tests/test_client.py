f147e2d3712314c60529bfbf869ff9a56d0abaf3
bereket-kume <bereketkume@gmail.com>
2024-08-26 11:14:39 +0300

    test has_license method

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -49,7 +49,7 @@ class TestGithubOrgClient(unittest.TestCase):
         ]
         with patch.object(
             GithubOrgClient, '_public_repos_url', new_callable=MagicMock
-            ) as mock_public_repos_url:
+        ) as mock_public_repos_url:
             mock_public_repos_url.return_value = (
                 "https://api.github.com/orgs/google/repos"
             )
@@ -60,3 +60,12 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_get_json.assert_called_once_with(
                 "https://api.github.com/orgs/google/repos"
                 )
+            
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """Test the has_license method"""
+        result = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(result, expected)
